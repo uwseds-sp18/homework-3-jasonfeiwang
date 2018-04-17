81976cdb4e5cf62dec00febce52c5ecd92132726
Fei Wang <jason.feiwang@gmail.com>
2018-04-16 22:12:04 -0700

    homework3 first version

---
+++ homework3.py
@@ -0,0 +1,33 @@
+#!/home/fei/anaconda3/bin/python
+
+import sys
+import pandas as pd
+import sqlite3
+from urllib.request import pathname2url
+
+sys.tracebacklimit=0
+
+def create_dataframe(inputDb):
+    try:
+        dburi = 'file:{}?mode=rw'.format(pathname2url(inputDb))
+        conn = sqlite3.connect(dburi, uri=True)
+
+        df = pd.read_sql_query("""
+        select video_id, category_id, 'us' as language from USvideos
+        union all 
+        select video_id, category_id, 'gb' as language from GBvideos 
+        union all 
+        select video_id, category_id, 'fr' as language from FRvideos
+        union all 
+        select video_id, category_id, 'de' as language from DEvideos
+        union all 
+        select video_id, category_id, 'ca' as language from CAvideos
+        """
+        , conn)
+        return(df) 
+    except Exception as ex:
+        if type(ex).__name__ == 'OperationalError':
+            #raise ValueError("Invalid path to a database.")
+            return(ex)
+
+
---
+++ test_homework3.py
@@ -0,0 +1,30 @@
+from homework3 import create_dataframe 
+import unittest 
+
+class TestHomework3(unittest.TestCase):
+    # test whether the dataframe returned contains the right column names
+    def test_column_names(self):
+        inputdf = create_dataframe('./class.db')
+        inputdfColumns, testColumns = list(inputdf.columns), ['video_id', 'category_id', 'language']
+        checkColumnInd = not bool(set(inputdfColumns).difference(set(testColumns))) 
+        self.assertTrue(checkColumnInd)
+
+    # test whether the dataframe returned contains the right number of rows
+    def test_table_rows(self):
+        inputdf = create_dataframe('./class.db')
+        self.assertTrue(inputdf.shape[0] == 75005)
+    
+    # test whether the columns ['video_id', 'category_id' and 'language'] is a key
+    def test_key_columns(self):
+        inputdf = create_dataframe('./class.db')
+        rows = inputdf.shape[0]
+        uniqueValuesOfColumns = inputdf.groupby(['video_id', 'category_id', 'language']).size().shape[0]
+        self.assertTrue(rows == uniqueValuesOfColumns)
+    
+    # test whether the correct exception is generated when an invalid path is provided.
+    def test_exception_type(self):
+        ex = create_dataframe('wrong_path')
+        self.assertTrue(type(ex).__name__ == 'OperationalError')
+
+if __name__ == '__main__':
+    unittest.main()
